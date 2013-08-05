80142a91d79af5797ffbcd04bc609fa680db5e23
cospan <cospan@gmail.com>
2013-08-05 19:16:50 -0400

    Changed behavior cy_renumeration script
    Now when one of the PIDs is found the script exits, this will fix
    contention between different programs vying for the same USB device

--- config/cy_renumerate.sh
+++ config/cy_renumerate.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 pid=`pidof cyusb_linux`
 
+#Call the standard cypress controller
 if [ "$pid" ]; then
     kill -s SIGUSR1 $pid
-fi
-
-pid=`pidof -x prometheus_usb.py`
-if [ "$pid" ]; then
-    kill -s SIGUSR1 $pid
+    exit
 fi
 
 pid=`pidof -x prometheus.py`
 if [ "$pid" ]; then
     kill -s SIGUSR1 $pid
+    exit
 fi
+
+#This is only used for debug
+pid=`pidof -x prometheus_usb.py`
+if [ "$pid" ]; then
+    kill -s SIGUSR1 $pid
+fi
+
 
