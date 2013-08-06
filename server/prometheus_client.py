17a5027a46d427b1754d42f54dddb22fbfec59c1
cospan <cospan@gmail.com>
2013-08-06 15:08:32 -0400

    Server and client are both working

--- server/prometheus_client.py
+++ server/prometheus_client.py
@@ -21,6 +21,22 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
+import sys
+import os
+import socket
+import time
+import re
+
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                os.pardir))
+
+from defines import STATUS_PORT
+from defines import DATA_PORT
+
+MAX_READ_SIZE = 1024
+
+class PrometheusClientError(Exception):
+    pass
 
 class PrometheusClient(object):
     """
@@ -28,4 +44,128 @@ class PrometheusClient(object):
     """
     def __init__(self):
         super (PrometheusClient, self).__init__()
+
+
+
+    def send_data(self, data = None, host = 'localhost', port = DATA_PORT):
+        """
+        Send data to the connected server
+
+        Args:
+            data (buffer): Character string
+            host (string/IP): Address of the host
+                (default = 'localhost' or this computer)
+            port (integer): server port on the local host
+                (default = default.DATA_PORT probably 0xC595)
+
+
+        Return:
+            Nothing
+
+        Raises:
+            PrometheusClientError
+                -Data is None
+                -Failed to connect to the server
+                -Transfer Failure
+        """
+        if data is None:
+            raise PrometheusClientError("Data cannot be set to None")
+
+        client = None
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #For the times where the socket was not closed cleanly
+            client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            client.connect((host, port))
+
+        except socket.error, err:
+            raise PrometheusClientError("Failed to connecto to server")
+
+        #Check for greeting
+        self.client.settimeout(1)
+        greeting = self.client.recv(MAX_READ_SIZE)
+        if len(greeting > 0):
+            print "Received from host: %s" % greeting
+
+        #Attempt to send the data
+        try:
+            self.client.sendall(data)
+        except socket.error, err:
+            raise PrometheusClientError("Failed to send data: %s" % str(err))
+
+        #I need a way to get feedback from the device
+
+        #Close the socket connection
+        self.client.close()
+        
+ 
+def get_server_status(host = 'localhost', port = STATUS_PORT):
+    """
+    Opens up a connection to the status server and request status
+
+    Args:
+        host (string/IP): Address of the host
+            (default = 'localhost' or this computer)
+        port (integer): server port on the local host
+            (default = default.STATUS_PORT probably 0xC594)
+    Return:
+        (String) Status
+
+    Raises:
+        Nothing
+    """
+    client = None
+    status = ""
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #For the times where the socket was not closed cleanly
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        client.connect((host, port))
+
+    except socket.error, err:
+        if re.search("Errno.*111", str(err)):
+            return "No Server Running"
+
+        return "Not Connected to Server: %s" % str(err)
+
+    #Check for greeting
+    client.settimeout(1)
+    try:
+        status = client.recv(MAX_READ_SIZE)
+
+    except socket.error as err:
+        print "Error reading data"
+
+    #Close the socket connection
+    client.shutdown(socket.SHUT_RDWR)
+    client.close()
+    return status     
+
+def is_server_running():
+    """
+    Check to see if there is a server already running
+
+    Opens up a socket, waits for a response
+    """
+    client = None
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(('localhost', STATUS_PORT))
+    except socket.error, err:
+        #print "Socket Error: %s" % str(err)
+        return False
+
+    try:
+        data = client.recv(MAX_READ_SIZE)
+        #client.send("Does this work??")
+        #print "Data Read from Host: %s" % str(data)
+        client.shutdown(socket.SHUT_RDWR)
+        client.close()
+    except socket.error as err:
+        #print "Is Server Running? Socket Error: %s" % str(err)
+        return False
+
+    return True
+    
+
 
--- server/prometheus_server.py
+++ server/prometheus_server.py
@@ -27,35 +27,17 @@ import socket
 import threading
 import Queue
 import select
+import signal
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__),
                 os.pardir))
 
 from defines import VERSION
-from defines import PORT
-
-
-
-
-# Echo server program
-#
-#HOST = ''                 # Symbolic name meaning all available interfaces
-#PORT = 50007              # Arbitrary non-privileged port
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST, PORT))
-#s.listen(1)
-#conn, addr = s.accept()
-#print 'Connected by', addr
-#while 1:
-#        data = conn.recv(1024)
-#            if not data: break
-#                conn.sendall(data)
-#                conn.close()
-#
+from defines import STATUS_PORT
+from defines import DATA_PORT
 
 MAX_READ_SIZE = 1024
-
 
 class SocketThread(threading.Thread):
     def __init__(self, sock, listen_port, queue, connection_cb, activity_cb):
@@ -66,7 +48,11 @@ class SocketThread(threading.Thread):
         self.connection_cb = connection_cb
         self.activity_cb = activity_cb
         self.queue = queue
-        conn = None
+
+        #signal.signal(signal.SIGUSR2, self.user2_event)
+
+    #def user2_event(self, signal_number, frame):
+    #    print "User 2 Event"
 
     def run(self):
         self.sock.bind(('', self.listen_port))
@@ -77,125 +63,197 @@ class SocketThread(threading.Thread):
                 self.sock.listen(1)
                 
                 conn, addr = self.sock.accept()
-                #print "Connected by: %s" % str(addr)
+                print "Connected by: %s" % str(addr)
                 self.connection_cb(conn)
+                ready = True
                 
-                
-                while True:
+                while ready:
                     try:
                         input_ready, output_list, except_list = select.select([conn], [], [])
                         
                         for s in input_ready:
                             data = s.recv(MAX_READ_SIZE)
                             if not data:
-                                #print "Closed Connection"
+                                print "Closed Connection"
+                                conn.shutdown(socket.SHUT_RDWR)
                                 conn.close()
                                 self.activity_cb(None)
-                        
+                                ready = False
+                                break
                             else:
                                 #print "Received Data"
-                                self.queue.put(data)
-                                self.activity_cb(conn)
+                                if self.queue is not None:
+                                    self.queue.put(data)
+                                if self.activity_cb is not None:
+                                    self.activity_cb(conn)
+
                     except select.error, err:
-                        #print "Select Error"
+                        print "Select Error"
                         break
 
             except socket.error, err:
                 #print "Socket Error: %s" % str(err)
                 return
 
+class PrometheusServerError(Exception):
+    pass
 
 class PrometheusServer(object):
     """
     Local Socket server used to facilitate inter process communication
     """
-    def __init__(self, server_status_cb = None, server_activity_cb = None):
+    def __init__(self,
+                 data_status_cb = None,
+                 data_activity_cb = None,
+                 status_connection_cb = None,
+                 status_activity_cb = None):
         super (PrometheusServer, self).__init__()
-        self.message = "Prometheus Server Version: %s" % VERSION
-        self.server_status_cb = server_status_cb
-        self.server_activity_cb = server_activity_cb
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.queue = Queue.Queue()
-        self.opened = False
+        self.greeting_message = "Prometheus Server Version: %s" % VERSION
 
+        #Get the callbacks
+        self.data_status_cb = data_status_cb
+        self.data_activity_cb = data_activity_cb
+        self.status_connection_cb = status_connection_cb
+        self.status_activity_cb = status_activity_cb
+
+
+        #Start the status server
+        self.status_queue = Queue.Queue()
+        self.status_connection = None
+        self.status_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.status_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.status_socket_thread = SocketThread(self.status_sock,
+                                                 STATUS_PORT,
+                                                 self.status_queue,
+                                                 self.server_status_connection_cb,
+                                                 self.server_status_activity_cb)
+
+
+
+        #Start the data server
+        self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.data_connection = None
+        self.data_queue = Queue.Queue()
         #SO_REUSADDR is nice because if there is a mistake I'm not stuck waiting
         #For the socket to time out
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.data_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        self.socket_thread = SocketThread(self.sock,
-                                          PORT,
-                                          self.queue,
-                                          self.connection_cb,
-                                          self.activity_cb)
+        self.data_socket_thread = SocketThread(self.data_sock,
+                                               DATA_PORT,
+                                               self.data_queue,
+                                               self.server_data_connection_cb,
+                                               self.server_data_activity_cb)
 
-        self.socket_thread.start()
+        self.status_socket_thread.start()
+        self.data_socket_thread.start()
 
 
     def send_greeting(self, connection):
         #Send greeting
-        connection.sendall(self.message)
+        connection.sendall(self.greeting_message)
 
-    def connection_cb(self, connection):
-        self.send_greeting(connection)
-        self.opened = True
-        if self.server_status_cb is not None:
-            self.server_status_cb(True)
+    def server_data_connection_cb(self, connection):
+        #self.send_greeting(connection)
+        self.send("Ready")
+        self.data_connection = connection
+        if self.data_status_cb is not None:
+            self.data_status_cb(True)
 
-    def activity_cb(self, connection):
+    #Data Server Callback for incomming data
+    def server_data_activity_cb(self, connection):
         if connection is None:
             #print "Socket Closed"
-            self.opened = False
-            if self.server_status_cb is not None:
-                self.server_status_cb(False)
-
-
+            self.data_connection = None
+            if self.data_status_cb is not None:
+                self.data_status_cb(False)
         else:
-            data = self.queue.get()
-            if self.server_activity_cb is not None:
-                self.server_activity_cb(data)
+            data = self.data_queue.get()
+            if self.data_activity_cb is not None:
+                self.data_activity_cb(data)
             #print "Received Data: %s" % str(data)
 
     def send(self, data):
-        if self.opened:
-            self.sock.send(data)
+        if self.data_connection:
+            self.data_sock.sendall(data)
         else:
             raise PrometheusServerError("Server is not connected")
 
+    #Status Server Callback for incomming data
+    def server_status_connection_cb(self, connection):
+        print "Status Connected"
+        self.status_connection = connection
+
+        if self.status_connection_cb:
+            self.status_connection_cb(connection)
+
+    def server_status_activity_cb(self, connection):
+        print "New data on the status queue"
+        if connection is None:
+            #Socket Closed
+            self.status_connection = None
+            if self.status_connection_cb:
+                self.status_connection_cb(False)
+        else:
+            data = self.data_queue.get()
+            if self.status_activity_cb is not None:
+                self.status_activity_cb(data)
+
+    def send_status(self, main_status):
+        print "Send status of a build"
+        status = self.greeting_message
+        status += " Data Port: 0x%X\n" % DATA_PORT
+        status += main_status
+
+        if self.status_connection is not None:
+            self.status_connection.sendall(status)
+        else:
+            raise PrometheusServerError("Status Server is not connected")
+
+
     def shutdown(self):
-        #print "Shutdown"
-        self.sock.close()
-        self.socket_thread.join()
+        #print "Shutdown Servers"
+        #Shutdown Data socket
+        if self.data_connection is not None:
+            self.data_connection.shutdown(socket.SHUT_RDWR)
+            self.data_connection.close()
+            self.data_connection = None
 
-def is_server_running():
-    """
-    Check to see if there is a server already running
+        #print "Closed Data Connection Socket"
 
-    Opens up a socket, write "Ping" and wait for a response
-    """
-    client = None
-    try:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(('localhost', PORT))
-    except socket.error as err:
-        #print "Socket Error: %s" % str(err)
-        return False
+        if self.data_sock is not None:
+            self.data_sock.shutdown(socket.SHUT_RDWR)
+            self.data_sock.close()
+            self.data_sock = None
 
-    try:
-        data = client.recv(MAX_READ_SIZE)
-        #client.send("Does this work??")
-        #print "Data Read from Host: %s" % str(data)
-        client.close()
-    except socket.error as err:
-        #print "Is Server Running? Socket Error: %s" % str(err)
-        return False
+        #print "Closed Data Server Socket"
 
-    return True
-    
+        if self.data_socket_thread is not None:
+            self.data_socket_thread.join()
+            self.data_socket_thread = None
 
+        #print "Data Server Thread Shutdown"
 
+        #Shutdown status socket
+        if self.status_connection is not None:
+            self.status_connection.shutdown(socket.SHUT_RDWR)
+            self.status_connection.close()
+            self.status_connection = None
 
+        #print "Closed Status Connection Socket"
 
+        if self.status_sock is not None:
+            self.status_sock.shutdown(socket.SHUT_RDWR)
+            self.status_sock.close()
+            self.status_socket = None
 
-if __name__ == "__main__":
-    print "HI"
+        #print "Closed Status Server Socket"
+
+        if self.status_socket_thread is not None:
+            self.status_socket_thread.join()
+            self.status_socket_thread = None
+
+        #print "Status Server Thread Shutdown"
+
+        #print "Both Servers are shutdown"
+
 
